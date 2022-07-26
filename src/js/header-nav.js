0e0a415f4fdf4054736ff278279c8bf1697fb2db
Greg McMullen <mcmullen.greg@gmail.com>
2022-07-25 22:37:40 -0400

    Playground for the WebComponent POC
    - Introduces a navigation pattern for the top-header
    - Establishes a minor baseline for WebComponent layouts
    
    ToDo:
    
    - Wrap the entire header as a web componenet, possibly with a slot
    for the title and navigation.
    - Continue basic development work on the bootstrapless layout

---
+++ src/js/header-nav.js
@@ -0,0 +1,29 @@
+class UMKCNavigation extends HTMLElement {
+  constructor() {
+    super();
+    this.links = {};
+    this.class = "";
+  }
+  connectedCallback() {
+    this.class = this.class;
+    this.links = JSON.parse(this.getAttribute("links"));
+    this.innerHTML = this.links;
+    // Custom function to render the component!
+    this.renderComponent();
+  }
+
+  renderComponent() {
+    this.innerHTML = `
+      <nav class="${this.class}">
+        <ul>
+          ${this.links.map(function(link){
+            console.log(typeof link.href);
+            let fullLink = (link.href !== "" ? '<a href="'+link.href+'">' + link.title + '</a>' : '<span>'+link.title+'</span>' );
+            return '<li>'+fullLink+'</li>'
+          }).join('')}
+        </ul>
+      </nav>
+    `
+  }
+}
+customElements.define('umkc-navigation', UMKCNavigation);
