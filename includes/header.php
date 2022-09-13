7fb4f7b77cfcd221c4c549cc19f5432553946865
Greg McMullen <mcmullen.greg@gmail.com>
2022-09-13 14:40:41 -0400

    Removes items removed from the design build

--- includes/header.php
+++ includes/header.php
@@ -1,34 +1,24 @@
 <header>
   <div class="bg-umkc-blue stripes header">
-    <h1 class="site-title"><a class="text-white" href="/">University of Missouri - Kansas City</a></h1>
-    <nav class="header__nav" aria-label="header__nav">
-      <ul class="nav nav--inline">
-        <?php
-          $links = array(
-            [ "href" => "#", "title" => "Quick Links"],
-            [ "href" => "#", "title" => "To Do"],
-            [ "href" => "", "title" => "Important Stuff"],
-            [ "href" => "", "title" => "|"],
-            [ "href" => "#", "title" => "Audience"],
-            [ "href" => "#", "title" => "Gateway"],
-            [ "href" => "#", "title" => "Type"],
-            [ "href" => "#", "title" => "Links"]
-          );
-          include 'nav_links.php';
-        ?>
-      </ul>
-    </nav>
+    <h1 class="site-title"><a href="/">University of Missouri - Kansas City</a></h1>
   </div>
-  <div class="bg-umkc-blue header header--secondary">
+  <div class="header header--secondary">
     <div class="logo">
       <a href="/" class="logo--link">
         <svg class="logo--image" aria-label="University of Missouri. Kansas City." role="img"><use xlink:href="#umkc"></use></svg>
       </a>
     </div>
-    <button class="menu o-btn main-navigation__menu" data-toggle=".main-navigation">
-      <svg class="main-navigation__icon main-navigation__icon--menu" aria-label="Open navigation menu">
-        <use xlink:href="#menu"></use>
-      </svg>
+    <div class="dept-header">
+      <h2 class="dept-header__title">
+        Office of the Chancellor
+      </h2>
+    </div>
+    <form class="search" action="https://www.umkc.edu/search.html">
+      <input name="q" />
+      <button class="o-btn">
+        <svg class="icon"><use xlink:href="#search" /></svg>
+      </button>
+    </form>
   </div>
   <?php include 'navigation.php'; ?>
 </header>
