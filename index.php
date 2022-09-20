e08b20c5fdf18b9b095fbb2c4d7421da67a58a99
Greg McMullen <mcmullen.greg@gmail.com>
2022-09-20 16:34:24 -0400

    Removes the structure from the index page, and places it in includes

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <?php include_once("includes/head.php"); ?>
+	<link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.core.min.css" />
+	<link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.theme.min.css">
+
   <body>
 		<!-- Load SVGs first? -->
 		<?php include_once("includes/svg-sprites.php"); ?>
@@ -16,7 +19,7 @@
 				</div>
 			</div>
 
-			<div class="breadcrumbs stripes--dark">
+			<nav class="breadcrumbs stripes--dark">
 				<ul class="nav nav--inline breadcrumbs__list">
 					<li class="breadcrumbs__item">
 						<a class="breadcrumbs__link" href="#">Home</a>
@@ -25,182 +28,53 @@
 						<a class="breadcrumbs__link" href="#">Thing</a>
 					</li>
 				</ul>
-			</div>
+			</nav>
 
-			<div class="content-row">
-				<div class="benefits-list">
-					<img alt="" class="benefits-list__image" src="https://picsum.photos/seed/picsum/448/535" loading="lazy" />
-					<div class="benefits-list__group">
-						<h2>Benefits List with Photo</h2>
-						<p class="highlight">Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
+			<?php
+			$with_image = true;
+			include('includes/kitchen-sink/benefits-list.php');
 
-						<div class="benefits-list__points">
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">1</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon benefits-list__icon--flame">
-									<svg class="icon"><use xlink:href="#flame" /></svg>
-								</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">3</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-									<!-- Foreach.isLast() inside of Cascade code -->
-									<p><a href="#" class="button button--arrow button--umkc-yellow">Hello there!</a></p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			$with_image = false;
+			include('includes/kitchen-sink/benefits-list.php');
 
-			<div class="content-row">
-				<h2 class="text-center">Benefits List without Photo</h2>
-				<p class="highlight text-center">Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-				<div class="benefits-list">
-					<div class="benefits-list__group">
-						<div class="benefits-list__points">
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">1</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">2</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">3</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="benefits-list__group">
-						<div class="benefits-list__points">
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">1</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">2</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item">
-								<div class="benefits-list__icon">3</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<p class="text-center"><a href="#" class="button button--arrow button--umkc-yellow">Hello there!</a></p>
-			</div>
+			$with_image = true;
+			include('includes/kitchen-sink/stats.php');
 
-			<div class="content-row">
-				<h2>Photo and Stats</h2>
-				<p class="highlight">Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-				<div class="benefits-list">
-					<img alt="" class="benefits-list__image" src="https://picsum.photos/seed/picsum/448/500" loading="lazy" />
-					<div class="benefits-list__group">
-						<div class="benefits-list--stats">
-							<div class="benefits-list__item benefits-list__item--stats">
-								<div class="benefits-list__stats">#1</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title benefits-list__title--light">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item benefits-list__item--stats">
-								<div class="benefits-list__stats">#2</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title benefits-list__title--light">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item benefits-list__item--stats">
-								<div class="benefits-list__stats">#3</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title benefits-list__title--light">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-							<div class="benefits-list__item benefits-list__item--stats">
-								<div class="benefits-list__stats">#4</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title benefits-list__title--light">Point</h3>
-									<p>Bacon ipsum dolor amet meatball jowl kielbasa pork belly buffalo. Cow pastrami burgdoggen spare ribs strip steak.</p>
-								</div>
-							</div>
-						</div>
+			$with_image = false;
+			include('includes/kitchen-sink/stats.php');
 
-					</div>
-				</div>
-			</div>
+			$with_image = true;
+			$bg_color = "umkc-blue";
+			$link_cta = true;
+			include('includes/kitchen-sink/cards.php');
 
-			<div class="content-row bg-umkc-gray">
-				<div class="benefits-list jc-evenly">
-					<div class="benefits-list__group">
-						<div class="benefits-list benefits-list--small-stats jc-evenly">
-							<div class="benefits-list__item benefits-list__item--stats">
-								<div class="benefits-list__stats">
-									<h2 class="benefits-list__title benefits-list__title--heavy">#1</h2>
-								</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title benefits-list__title--light">Point Pofdslhjs</h3>
-								</div>
-							</div>
-							<div class="benefits-list__item benefits-list__item--stats">
-								<div class="benefits-list__stats">
-									<h2 class="benefits-list__title benefits-list__title--heavy">#2</h2>
-								</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title benefits-list__title--light">Additional Content</h3>
-								</div>
-							</div>
-							<div class="benefits-list__item benefits-list__item--stats">
-								<div class="benefits-list__stats">
-									<h2 class="benefits-list__title benefits-list__title--heavy">#3</h2>
-								</div>
-								<div class="benefits-list__details">
-									<h3 class="benefits-list__title benefits-list__title--light">Lorem ipsum dolor</h3>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			$with_image = false;
+			$bg_color = false;
+			$link_cta = false;
+			$link = true;
+			include('includes/kitchen-sink/cards.php');
 
-			<?php include_once("includes/kitchen-sink/buttons.php"); ?>
+			$with_image = false;
+			$bg_color = false;
+			$link_cta = false;
+			$link = false;
+			include('includes/kitchen-sink/cards.php');
+
+			$count = 1;
+			include('includes/kitchen-sink/quotes.php');
+
+			$count = 3;
+			$footer_scripts = "";
+			include('includes/kitchen-sink/quotes.php');
+
+			## Buttons!
+			include_once("includes/kitchen-sink/buttons.php");
+		?>
 		</div>
 		<?php include_once("includes/footer.php"); ?>
 
 		<script src="/assets/js/main.js"></script>
+		<?php if ( $footer_scripts )
+			echo $footer_scripts; ?>
   </body>
 </html>
