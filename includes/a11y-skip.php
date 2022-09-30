f52adc8c14353f538eba8d107980b4e03982651d
Greg McMullen <mcmullen.greg@gmail.com>
2022-09-30 16:07:25 -0400

    Removes all php and lando components

--- includes/a11y-skip.php
+++
@@ -1,7 +0,0 @@
-<nav class="skip-links" aria-label="Skip Links">
-  <ul>
-    <li><a href="#content">Skip to Content</a></li>
-    <li><a href="#main-navigation">Skip to Navigation</a></li>
-    <li><a href="#search">Skip to Search</a></li>
-  </ul>
-</nav>
