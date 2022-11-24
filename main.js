b9b733f96b119bc02f86ab3d62ec5feda64f0138
Greg McMullen <mcmullen.greg@gmail.com>
2022-11-23 20:43:37 -0500

    Adds quote functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import '/src/scss/styles.scss'
 import '/src/scss/print.scss'
 import '/src/js/toggles.js'
+import '/src/js/quotes.js'
 // import javascriptLogo from './javascript.svg'
 // import { setupCounter } from './counter.js'
 
