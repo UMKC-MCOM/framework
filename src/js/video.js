852a12dba7a8016a68a027c9e4557b681df396cd
Greg McMullen <mcmullen.greg@gmail.com>
2023-03-24 12:37:44 -0400

    Video row code + video overlay

--- src/js/video.js
+++ src/js/video.js
@@ -2,7 +2,7 @@
 const heroVideo = document.querySelector("#hero-video");
 // Get the sources for the video
 if ( heroVideo !== null) {
-  const sources   = heroVideo.querySelectorAll("source");
+  var sources   = heroVideo.querySelectorAll("source");
 }
 // If there is a video, run the code!
 if ( heroVideo && sources.length != 0 ){
@@ -34,3 +34,57 @@ const videoButton = document.querySelectorAll("[data-video]");
     })
   });
 });
+
+// Code for overlayed videos within the HTML code.
+// Only works with the "SHARE" links from YouTube (e.g. youtu.be/[link_id])
+const vo = document.createElement("div");
+      vo.setAttribute("id", "video-overlay");
+const vb = document.createElement("button");
+      vb.classList.add("o-btn");
+      vb.classList.add("video__close");
+      vb.setAttribute("id", "close-video-overlay");
+      vb.setAttribute("aria-label", "Close Video Overlay")
+      vb.innerHTML = `<svg class="icon menu__icon"><use xlink:href="#close"></use></svg>`;
+vo.appendChild(vb);
+const vw = document.createElement("div");
+      vw.classList.add("video");
+const voc = document.createElement("div");
+      voc.classList.add("embed-container");
+      voc.innerHTML = `<div class="video-embed" id="player"><iframe frameborder="0" src="" allowfullscreen allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe></div>`;
+      vw.appendChild(voc);
+      vo.appendChild(vw);
+      document.body.appendChild(vo);
+// Searches the page for any youtu.be links
+const videoLinks = document.querySelectorAll(`a[href*="youtu.be"]`);
+[...videoLinks].forEach((videoLink) => {
+  videoLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    vo.classList.add("is-visible--flex");
+    let video = e.target.getAttribute("href");
+    let iframe = document.querySelector("#player iframe");
+    iframe.setAttribute("src", "//www.youtube.com/embed/"+getId(video));
+  })
+});
+// Click Event for using the video overlay to close
+vo.addEventListener("click", function(e) {
+  e.preventDefault();
+  vo.classList.remove("is-visible--flex");
+  let iframe = document.querySelector("#player iframe");
+  iframe.setAttribute("src", "");
+});
+// Click Event for using the close button
+vb.addEventListener("click", function(e) {
+  e.preventDefault();
+  vo.classList.remove("is-visible--flex");
+  let iframe = document.querySelector("#player iframe");
+  iframe.setAttribute("src", "");
+});
+
+// Snags the ID from the youtu.be URL.
+function getId(url) {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    const match = url.match(regExp);
+    return (match && match[2].length === 11)
+      ? match[2]
+      : null;
+}
