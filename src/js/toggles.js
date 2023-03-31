d4a36aa9ae20c98038a075a8df88511d0c6693d3
Greg McMullen <mcmullen.greg@gmail.com>
2023-03-31 15:42:56 -0400

    Fixes toggle functionality for closing open entities when switching toggle items

--- src/js/toggles.js
+++ src/js/toggles.js
@@ -2,13 +2,23 @@ const toggles = document.querySelectorAll('[data-toggle]');
 [...toggles].forEach((toggle) => {
   toggle.addEventListener('click', function(e){
     e.preventDefault();
+    let parent = toggle.parentNode.closest("div");
     let content = document.querySelector(toggle.dataset.toggle);
-    if ( content.getAttribute('aria-hidden') === "false" ) {
-      toggle.setAttribute('aria-expanded', false);
-      content.setAttribute('aria-hidden', true);
-    } else {
+    if ( content.getAttribute('aria-hidden') === null || content.getAttribute('aria-hidden') === "true" ) {
+      closeOpenToggles(parent);
       toggle.setAttribute('aria-expanded', true);
       content.setAttribute('aria-hidden', false);
+    } else {
+      closeOpenToggles(parent);
     }
   })
 });
+
+function closeOpenToggles(rootContainer) {
+  let openBtn = rootContainer.querySelectorAll('[aria-expanded=true]');
+  openBtn.forEach((openBtn) => {
+    let content = document.querySelector(openBtn.dataset.toggle);
+    openBtn.setAttribute('aria-expanded', false);
+    content.setAttribute('aria-hidden', true);
+  })
+}
