cd3859d9cbc391e24ad4e7512c7a041a37d2cbe4
wpa42c <wpa42c@umsystem.edu>
2023-04-18 09:50:27 -0500

    Squashed commit of the following:
    commit ac3a79ff00dc9e0bc1a5e6acc528acb76d4f8707
    Author: wpa42c <wpa42c@umsystem.edu>
    Date:   Tue Apr 18 09:37:55 2023 -0500
    
        Updating gitignore to include .vs files
    
    commit c19e3fa6e6eb56863f4f5eac17d23700bfe3f233
    Author: Gregory McMullen <mcmullengreg@users.noreply.github.com>
    Date:   Wed Apr 12 16:11:24 2023 -0400
    
        Storytelling Row (#52)
    
        * Updates the CSS to make sure the Video doesn't escape the content width
    
        * Adds the story telling row
    
        * remove padding on breadcrumbs
    
    commit ebea2969cfa43ead5557ecf4bffe84d7a314a22c
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:35:26 2023 -0400
    
        Text block and JS integrations
    
    commit e9351c9ef440fd338f235aed95781114bc369207
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:35:02 2023 -0400
    
        Adds tocBot
    
    commit d011c08e8c28969ce0ac34e57011e65c298326ab
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:32:46 2023 -0400
    
        Adds TOC build, and also removes teaser
    
    commit 92f1d0646c20f76e61cd24768f220baf9081e292
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:32:14 2023 -0400
    
        Updates to sprites
    
    commit ab32d704ef6f0e55904c5d5ae837cf9982982a8c
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:32:01 2023 -0400
    
        Navigation updates for long nav lists
    
    commit 35a3658d4a56fe53ec57c99133649be4ce2f58db
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:31:08 2023 -0400
    
        Button style update
    
    commit dfb66a26beea7e15492a2f655f43d3c83293661b
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:30:52 2023 -0400
    
        Slight news content alterations for links
    
    commit 70a525ce1b84b1cb8a2ec15a0ceda3c67ca3aded
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:30:19 2023 -0400
    
        Content update
    
    commit 71721508108093c19e67392dca31af68b2c9b1ea
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:29:42 2023 -0400
    
        Shows what links look like vs buttons
    
    commit e1cc22a3f94bf3b7377eef2301966eb6cc6ae324
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:29:16 2023 -0400
    
        Alters button styles
    
    commit 7f19467eac20779fb289b227ef49cba45b539fc3
    Author: Greg McMullen <mcmullen.greg@gmail.com>
    Date:   Thu Apr 6 13:28:55 2023 -0400
    
        Alters button to be yellow vs white outline

---
+++ src/js/toc.js
@@ -0,0 +1,26 @@
+function makeIds () { // eslint-disable-line
+  var content = document.querySelector('.standard--content')
+  var headings = content.querySelectorAll('h1, h2, h3, h4, h5, h6, h7')
+  var headingMap = {}
+
+  Array.prototype.forEach.call(headings, function (heading) {
+    var id = heading.id
+      ? heading.id
+      : heading.textContent.trim().toLowerCase()
+        .split(' ').join('-').replace(/[!@#$%^&*():]/ig, '').replace(/\//ig, '-')
+    headingMap[id] = !isNaN(headingMap[id]) ? ++headingMap[id] : 0
+    if (headingMap[id]) {
+      heading.id = id + '-' + headingMap[id]
+    } else {
+      heading.id = id
+    }
+  })
+}
+if ( document.getElementById('standard-page') !== null ) {
+  makeIds()
+  tocbot.init({
+    tocSelector: '.js-toc',
+    contentSelector: '.standard--content',
+    headingSelector: 'h1, h2, h3, h4, h5, h6',
+  })
+}
