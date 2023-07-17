c7806af599200968953471f912200f7ffd37f166
Greg McMullen <mcmullen.greg@gmail.com>
2023-07-17 16:58:11 -0400

    Cleanup

--- src/js/alerts.js
+++ src/js/alerts.js
@@ -1,28 +1,4 @@
 // $.ajax({
-//   url: "https://content.getrave.com/rss/umkc/channel1",
-//   cache: false
-// }).done(function(d){
-//   console.log(d);
-//   $(d).find("item").each(function(){
-//     var item = $(this);
-//     var title = item.find("title").text();
-//     var desc  = item.find("description").text();
-//     var pubDate = item.find("pubDate").text();
-//     var myDate = new Date(pubDate);
-//     var expDate = new Date(pubDate);
-//     var exp  = expDate.setDate(expDate.getDate() + 2);
-//     var xDate = new Date(exp)
-//     var html;
-//     if ( new Date() <= exp ){
-//       html = "<div class='alert alert--danger' role='alert' aria-atomic='true'><div class='alert--danger__icon'><span class='fa' aria-hidden='true'></span></div><div class='alert__content'>";
-//       html += "<p><strong>"+title+"</strong><br />"+ desc +"<br/>Posted: " +  myDate.toLocaleDateString() + "</p>";
-//       html += "</div></div>";
-//     }
-//     $('.alertme').append(html);
-//   });
-// });
-//
-// $.ajax({
 //   url: "https://www.getrave.com/rss/xavier/channel2",
 //   cache: false
 // }).done(function(d){
@@ -34,39 +10,16 @@
 //     var pubDate = item.find("pubDate").text();
 //     var myDate = new Date(pubDate);
 //     var expDate = new Date(pubDate);
-//     var exp  = expDate.setDate(expDate.getDate() + 45);
+//    Other days can also be added to the setDate function
+//     var exp  = expDate.setDate(expDate.getDate() + 2);
 //     var xDate = new Date(exp)
 //     var html;
 //     if ( new Date() <= exp ){
-//       html = "<div class='alert alert--warning' role='alert' aria-atomic='true'><div class='alert--warning__icon'><span class='fa' aria-hidden='true'></span></div><div class='alert__content'>";
+//       html = "<div class='alert alert--danger' role='alert' aria-atomic='true'><div class='alert--danger__icon'><span class='fa' aria-hidden='true'></span></div><div class='alert__content'>";
 //       html += "<p><strong>"+title+"</strong><br />"+ desc +"<br/>Posted: " +  myDate.toLocaleDateString() + "</p>";
 //       html += "</div></div>";
 //     }
-//     $('.twn-alert').append(html);
-//   });
-// });
-//
-// $.ajax({
-//   url: "https://www.getrave.com/rss/xavier/channel3",
-//   cache: false
-// }).done(function(d){
-//   console.log(d);
-//   $(d).find("item").each(function(){
-//     var item = $(this);
-//     var title = item.find("title").text();
-//     var desc  = item.find("description").text();
-//     var pubDate = item.find("pubDate").text();
-//     var myDate = new Date(pubDate);
-//     var expDate = new Date(pubDate);
-//     var exp  = expDate.setDate(expDate.getDate() + 45);
-//     var xDate = new Date(exp)
-//     var html;
-//     if ( new Date() <= exp ){
-//       html = "<div class='alert alert--info' role='alert' aria-atomic='true'><div class='alert--info__icon'><span class='fa' aria-hidden='true'></span></div><div class='alert__content'>";
-//       html += "<p><strong>"+title+"</strong><br />"+ desc +"<br/>Posted: " +  myDate.toLocaleDateString() + "</p>";
-//       html += "</div></div>";
-//     }
-//     $('.psn-alert').append(html);
+//     $('.alertme').append(html);
 //   });
 // });
 
