e7aab4bc2db9a258394494ddd2417507ae7b8eaa
Greg McMullen <mcmullen.greg@gmail.com>
2023-07-17 12:34:41 -0400

    Adds LS for hiding the GDPR banner

--- src/js/alerts.js
+++ src/js/alerts.js
@@ -70,3 +70,16 @@
 //   });
 // });
 
+const gdprBanner = document.querySelector(".gdpr");
+const cookieConsent = gdprBanner.querySelector(".banner-accept");
+const bannerClosed = localStorage.getItem("bannerClosed");
+cookieConsent.addEventListener("click", function(){
+  localStorage.setItem("bannerClosed", true);
+  checkBanner()
+});
+function checkBanner() {
+  if ( bannerClosed ) {
+    gdprBanner.style.display = "none";
+  }
+}
+checkBanner()
