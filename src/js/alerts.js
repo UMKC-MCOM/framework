a550591b73d64e78990d557d228027ff88a7dfb2
Greg McMullen <mcmullen.greg@gmail.com>
2023-07-17 14:43:39 -0400

    Updates the alerts and JS structure
    - Adds alert lookup for the Rave system with logic
    - Moves the consent JS to a new file
    - Fixes some of the display styles after working with test data

--- src/js/alerts.js
+++ src/js/alerts.js
@@ -1,5 +1,5 @@
 // $.ajax({
-//   url: "https://www.getrave.com/rss/xavier/channel1",
+//   url: "https://content.getrave.com/rss/umkc/channel1",
 //   cache: false
 // }).done(function(d){
 //   console.log(d);
@@ -70,16 +70,39 @@
 //   });
 // });
 
-const gdprBanner = document.querySelector(".gdpr");
-const cookieConsent = gdprBanner.querySelector(".banner-accept");
-const bannerClosed = localStorage.getItem("bannerClosed");
-cookieConsent.addEventListener("click", function(){
-  localStorage.setItem("bannerClosed", true);
-  checkBanner()
-});
-function checkBanner() {
-  if ( bannerClosed ) {
-    gdprBanner.style.display = "none";
-  }
+function checkAlerts() {
+  let header = document.querySelector("header");
+  let alertDiv = document.createElement("div");
+      alertDiv.className = "site-alert";
+      alertDiv.innerHTML = `<svg class="icon site-alert__icon"><use xlink:href="#info"></use></svg><div class="site-alert__content">{{content}}</div>`;
+  fetch("https://content.getrave.com/rss/umkc/channel1")
+  .then((response) => {
+    if (response.ok) {
+      return response.text();
+    }
+    return Promise.reject(response);
+  })
+  .then((str) => { new window.DOMParser().parseFromString(str, "text/xml") })
+  .then((data) => {
+    const items = data.querySelectorAll("item");
+    items.forEach( el => {
+      let title = el.querySelector("title").innerHTML;
+      let desc = el.querySelector("description").innerHTML;
+      let pubDate = el.querySelector("pubDate").innerHTML;
+      let link = el.querySelector("link").innerHTML;
+      let expDate = new Date(pubDate);
+          expDate = expDate.setDate(expDate.getDate() + 2);
+      // If the Current date is less than the expiration date
+      if ( new Date() <= expDate) {
+        let alertContent = `<h1 class="site-alert__title">${title}</h1><p>${desc}</p>`;
+        if ( link ) {
+          alertContent += `<p>Additional <a href="${link}">updates and emergency details</a>.</p>`
+        }
+        alertDiv.innerHTML = alertDiv.innerHTML.replace(`{{content}}`, alertContent);
+        header.insertAdjacentElement('beforebegin', alertDiv);
+      }
+    })
+  })
+  .catch((error) => {})
 }
-checkBanner()
+checkAlerts()
