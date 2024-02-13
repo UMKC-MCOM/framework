72829d71798ae01ca3bc3486e823693eb409df53
Will Angles <wpa42c@umsystem.edu>
2024-02-13 11:05:37 -0600

    Slate Form Installed

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ import '/src/js/directory.js'
 import '/src/js/resource.js'
 import '/src/js/alerts.js'
 import '/src/js/consent.js'
+import '/src/js/infopopup.js'
 // import javascriptLogo from './javascript.svg'
 // import { setupCounter } from './counter.js'
 
