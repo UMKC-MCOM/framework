a9757fc3b38c5e5eaa48f78a2b2aab31eada75ea
Greg McMullen <mcmullen.greg@gmail.com>
2024-02-29 16:39:41 -0500

    Get rid of some errors

--- src/js/infopopup.js
+++ src/js/infopopup.js
@@ -4,25 +4,29 @@ const formClose = document.getElementById('getInfoClose');
 const formScreen = document.getElementById('getInfoFormScreen');
 
 // Open the Get More Info form when the button is clicked.
-button.addEventListener('click', function(e) {
-    e.preventDefault();
-    form.classList.remove('hidden');
-    formScreen.classList.remove('hidden');
-    button.classList.add('hidden');
-});
-
+if ( button != null ) {
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        form.classList.remove('hidden');
+        formScreen.classList.remove('hidden');
+        button.classList.add('hidden');
+    });
+}
+if ( formClose != null ) {
 // Close the Get More Info form when the X is clicked.
-formClose.addEventListener('click', function(e) {
-    e.preventDefault();
-    form.classList.add('hidden');
-    formScreen.classList.add('hidden');
-    button.classList.remove('hidden');
-});
-
-// Close the Get More Info form when the smokescreen is clicked.
-formScreen.addEventListener('click', function(e) {
-    e.preventDefault();
-    form.classList.add('hidden');
-    formScreen.classList.add('hidden');
-    button.classList.remove('hidden');
-});
+    formClose.addEventListener('click', function(e) {
+        e.preventDefault();
+        form.classList.add('hidden');
+        formScreen.classList.add('hidden');
+        button.classList.remove('hidden');
+    });
+}
+if ( formScreen != null ) {
+    // Close the Get More Info form when the smokescreen is clicked.
+    formScreen.addEventListener('click', function(e) {
+        e.preventDefault();
+        form.classList.add('hidden');
+        formScreen.classList.add('hidden');
+        button.classList.remove('hidden');
+    });
+}
