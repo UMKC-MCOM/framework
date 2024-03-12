8a3ef871d669014f7e7840faf3480e8fc35dbc07
Will Angles <wpa42c@umsystem.edu>
2024-03-12 15:17:39 -0500

    Restyled certain form elements to look better on mobile & tablet

--- src/js/infopopup.js
+++ src/js/infopopup.js
@@ -1,32 +1,22 @@
-const button = document.getElementById('getInfoButton');
-const form = document.getElementById('getInfoForm');
-const formClose = document.getElementById('getInfoClose');
-const formScreen = document.getElementById('getInfoFormScreen');
+const giButton = document.getElementById('getInfoButton');
+const giForm = document.getElementById('getInfoForm');
+const giFormClose = document.getElementById('getInfoClose');
+const giFormScreen = document.getElementById('getInfoFormScreen');
 
 // Open the Get More Info form when the button is clicked.
-if ( button != null ) {
-    button.addEventListener('click', function(e) {
+if ( giButton != null ) {
+    giButton.addEventListener('click', function(e) {
         e.preventDefault();
-        form.classList.remove('hidden');
-        formScreen.classList.remove('hidden');
-        button.classList.add('hidden');
+        giForm.setAttribute('aria-hidden', false);
+        giFormScreen.setAttribute('aria-hidden', false);
+        // giButton.setAttribute('aria-hidden', true); // Button doesn't need to hide, it's covered
     });
 }
-if ( formClose != null ) {
+if ( giFormClose != null ) {
 // Close the Get More Info form when the X is clicked.
-    formClose.addEventListener('click', function(e) {
+    giFormClose.addEventListener('click', function(e) {
         e.preventDefault();
-        form.classList.add('hidden');
-        formScreen.classList.add('hidden');
-        button.classList.remove('hidden');
-    });
-}
-if ( formScreen != null ) {
-    // Close the Get More Info form when the smokescreen is clicked.
-    formScreen.addEventListener('click', function(e) {
-        e.preventDefault();
-        form.classList.add('hidden');
-        formScreen.classList.add('hidden');
-        button.classList.remove('hidden');
+        giForm.setAttribute('aria-hidden', true);
+        giFormScreen.setAttribute('aria-hidden', true);
     });
 }
