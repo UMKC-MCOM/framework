b4b0484bd43347c7b27d654d2f3eb929058b98b5
Greg McMullen <mcmullen.greg@gmail.com>
2025-03-26 14:09:53 -0400

    Updates to the directory pagination

--- src/js/directory-pagination.js
+++ src/js/directory-pagination.js
@@ -1,36 +1,31 @@
-// loadScript("https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js", true)
-const dataSet = await fetch("https://www.umkc.edu/hr/profiles/index.json").then( response => response.json());
+const currPage = window.location.href;
+const directory = document.querySelector(".di") !== null ? document.querySelector(".di") : false;
+const perPage = 10;
 
-// console.log(_find(dataSet, ))
-
-
-let perPage = 8;
-const displayPageNav = perPage => {
-
+const displayPageNav = (dataSet, perPage) => {
   let pagination =``
   const totalItems = dataSet.length
   perPage = perPage ? perPage : 1
   const pages = Math.ceil(totalItems/perPage)
-
-  for(let i = 1; i <= pages; i++) {
-    pagination += `<a href="#">${i}</a>`
+  if ( pages > 1 ){
+    for(let i = 1; i <= pages; i++) {
+      pagination += `<a href="#">${i}</a>`
+    }
   }
-
   let pagNav = document.getElementById('pagination')
   pagNav.innerHTML = pagination;
   // pagNav.innerHTML = `showing ${perPage} of ${totalItems}`;
   pagNav.childNodes.forEach( item => {
     item.addEventListener("click", function(e){
       e.preventDefault();
-      displayItems(item.innerHTML,perPage);
+      document.querySelector(".di").scrollIntoView();
+      displayItems(dataSet, item.innerHTML,perPage);
     });
   });
 }
 
-const displayItems = ( page = 1, perPage = 2 ) => {
-
- let index, offSet
-
+const displayItems = ( dataSet, page = 1, perPage = 2 ) => {
+let index, offSet
   if(page == 1 || page <=0)  {
     index = 0
     offSet = perPage
@@ -45,16 +40,46 @@ const displayItems = ( page = 1, perPage = 2 ) => {
   const slicedItems = dataSet.slice(index, offSet)
 
   const html = slicedItems.map(item =>
-   `<div class="di__item">
+  `<div class="di__item">
       <img class="di__image" src="${item.image}" loading="lazy" alt="" />
       <p class="di__name"><a href="${item.link}">${item.name}</a></p>
-      <p class="di__title">${item.title}</p>
+      <p class="di__title">${item.title.toString()}</p>
       <p class="di__dept">${item.email}</p>
     </div>`)
 
   document.querySelector('.paginationDirectory').innerHTML = html.join('')
-
 }
 
-displayPageNav(perPage)
-displayItems(1, perPage)
+function filter(searchTerm, dataSet){ 
+  let term = searchTerm.toLowerCase();
+  dataSet = dataSet.filter( (e) => {
+    let name = e.name.toLowerCase();
+    let title = e.title.toString().toLowerCase();
+    let email = e.email.toLowerCase();
+    if ( name.includes(term) || title.includes(term) || email.includes(term)) {
+      return e;
+    }
+  });
+  
+  displayPageNav(dataSet, perPage)
+  displayItems(dataSet, 1, perPage)
+}
+
+if ( directory ) {
+  const dirFeed = currPage.includes(".html") ? currPage.replace("html", "json") : currPage + "index.json";
+  var dataSet = await fetch(dirFeed).then( response => { if ( response.ok ) response.json() }).catch((error) => {
+    console.log(error.message)
+  });
+  console.log(dataSet)
+  const search_form = document.getElementById("directory_search");
+  const search_input = document.getElementById("dirSearch");
+  if ( search_form !== null ) {
+    search_form.addEventListener("input", e => {
+      let search_t = e.target.value;
+      filter(search_t, dataSet);
+    });
+  }
+  
+  displayPageNav(dataSet, perPage);
+  displayItems(dataSet, 1, perPage);
+}
--- src/js/directory.js
+++ src/js/directory.js
@@ -1,31 +1,31 @@
-const directory = document.querySelector(".di") !== null ? document.querySelector(".di") : false;
-if ( directory ) {
-  const search_form = document.getElementById("directory_search");
-  const search_input = document.getElementById("dirSearch");
-  const profiles = document.querySelectorAll(".di__item:not(.di__item--header)");
-  const errorMessage = document.createElement("div");
-      errorMessage.classList = ("alert alert--info");
-      errorMessage.innerHTML = `<p>No results for your search term(s).</p>`;
-
-  const showProfiles = async(term) => {
-    errorMessage.remove();
-    [...profiles].forEach( profile => profile.classList.remove('hidden'))
-    if ( term.length >= 3 ) {
-      [...profiles]
-        .filter(profile => !profile.textContent.toLowerCase().includes(term.toLowerCase()) )
-        .forEach( profile => {
-          profile.classList.add('hidden') } )
-      let results = document.querySelectorAll(".di__item.hidden");
-      if ( results.length == profiles.length ){
-        directory.appendChild(errorMessage);
-      }
-    }
-  }
-
-  if ( search_form !== null ) {
-    search_form.addEventListener("input", e => {
-      let search_t = search_input.value;
-      showProfiles(search_t);
-    });
-  }
-}
+// const directory = document.querySelector(".di") !== null ? document.querySelector(".di") : false;
+// if ( directory ) {
+//   const search_form = document.getElementById("directory_search");
+//   const search_input = document.getElementById("dirSearch");
+//   const profiles = document.querySelectorAll(".di__item:not(.di__item--header)");
+//   const errorMessage = document.createElement("div");
+//       errorMessage.classList = ("alert alert--info");
+//       errorMessage.innerHTML = `<p>No results for your search term(s).</p>`;
+// 
+//   const showProfiles = async(term) => {
+//     errorMessage.remove();
+//     [...profiles].forEach( profile => profile.classList.remove('hidden'))
+//     if ( term.length >= 3 ) {
+//       [...profiles]
+//         .filter(profile => !profile.textContent.toLowerCase().includes(term.toLowerCase()) )
+//         .forEach( profile => {
+//           profile.classList.add('hidden') } )
+//       let results = document.querySelectorAll(".di__item.hidden");
+//       if ( results.length == profiles.length ){
+//         directory.appendChild(errorMessage);
+//       }
+//     }
+//   }
+// 
+//   if ( search_form !== null ) {
+//     search_form.addEventListener("input", e => {
+//       let search_t = search_input.value;
+//       showProfiles(search_t);
+//     });
+//   }
+// }
